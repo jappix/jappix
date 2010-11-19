45a36c6bbb8829b5393d0c9e6d2f4bb3c78e333d
Vanaryon <Vanaryon@6385bf5c-453f-4a1d-ac06-b509b7c32dfb>
2010-11-19 21:44:54 +0000

    jQuery Form added.
    git-svn-id: http://svn.codingteam.net/jappix/trunk@618 6385bf5c-453f-4a1d-ac06-b509b7c32dfb

---
+++ js/upload.js
@@ -0,0 +1,15 @@
+/*
+
+Jappix - An open social platform
+These are the upload JS scripts for Jappix
+
+-------------------------------------------------
+
+License: AGPL
+Author: Valérian Saliou
+Contact: http://project.jappix.com/contact
+Last revision: 19/11/10
+
+*/
+
+
