c3b90471b352908d8433b8f7659e5ce903117617
Vanaryon <Vanaryon@6385bf5c-453f-4a1d-ac06-b509b7c32dfb>
2011-04-20 14:16:46 +0000

    Added JSJaC dates.
    git-svn-id: http://svn.codingteam.net/jappix/trunk@1063 6385bf5c-453f-4a1d-ac06-b509b7c32dfb

--- js/jsjac.js
+++ js/jsjac.js
@@ -7,7 +7,7 @@ This is the JSJaC library for Jappix (from trunk)
 
 Licenses: Mozilla Public License version 1.1, GNU GPL, AGPL
 Authors: Stefan Strigler, Valérian Saliou, Zash
-Last revision: 02/04/11
+Last revision: 20/04/11
 
 */
 
