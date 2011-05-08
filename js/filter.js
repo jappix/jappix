4598faf9a035c6fbd569f9b87af88a0e284e3e54
Vanaryon <Vanaryon@6385bf5c-453f-4a1d-ac06-b509b7c32dfb>
2011-05-08 14:54:27 +0000

    New tab opened for XMPP links.
    git-svn-id: http://svn.codingteam.net/jappix/trunk@1140 6385bf5c-453f-4a1d-ac06-b509b7c32dfb

--- js/filter.js
+++ js/filter.js
@@ -34,7 +34,7 @@ function filterLinks(string, mode, style) {
 		target = '';
 	
 	// XMPP address
-	string = string.replace(/(\s|<br \/>|^)(([a-zA-Z0-9\._-]+)@([a-zA-Z0-9\.\/_-]+))(\s|$)/gi, '$1<a href="xmpp:$2"' + style + '>$2</a>$5');
+	string = string.replace(/(\s|<br \/>|^)(([a-zA-Z0-9\._-]+)@([a-zA-Z0-9\.\/_-]+))(\s|$)/gi, '$1<a href="xmpp:$2" target="_blank"' + style + '>$2</a>$5');
 	
 	// Simple link
 	string = string.replace(/(\s|<br \/>|^|\()((https?|ftp|file|xmpp|irc|mailto|vnc|webcal|ssh|ldap|smb|magnet|spotify)(:)([^<>'"\s\)]+))/gim, '$1<a href="$2"' + target + style + '>$2</a>');
