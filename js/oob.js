f207eae1c3e6bcd1d8dfefecee8815119972288b
Vanaryon <Vanaryon@6385bf5c-453f-4a1d-ac06-b509b7c32dfb>
2011-08-25 18:17:13 +0000

    Added OOB file to Jappix Desktop.
    git-svn-id: http://svn.codingteam.net/jappix/trunk@1276 6385bf5c-453f-4a1d-ac06-b509b7c32dfb

---
+++ js/oob.js
@@ -0,0 +1,14 @@
+/*
+
+Jappix - An open social platform
+These are the Out of Band Data JS scripts for Jappix
+
+-------------------------------------------------
+
+License: AGPL
+Author: Valérian Saliou
+Last revision: 25/08/11
+
+*/
+
+
