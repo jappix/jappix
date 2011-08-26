92c5b789c1b020c63b88597f776553f2e181055f
Vanaryon <Vanaryon@6385bf5c-453f-4a1d-ac06-b509b7c32dfb>
2011-08-26 16:12:18 +0000

    PHP send API added
    git-svn-id: http://svn.codingteam.net/jappix/trunk@1286 6385bf5c-453f-4a1d-ac06-b509b7c32dfb

---
+++ php/send.php
@@ -0,0 +1,168 @@
+<?php
+
+/*
+
+Jappix - An open social platform
+This is the Jappix Out of Band file send script
+
+-------------------------------------------------
+
+License: AGPL
+Author: Valérian Saliou
+Last revision: 26/08/11
+
+*/
+
+// PHP base
+define('JAPPIX_BASE', '..');
+
+// Get the needed files
+require_once('./functions.php');
+require_once('./read-main.php');
+require_once('./read-hosts.php');
+
+// Optimize the page rendering
+hideErrors();
+compressThis();
+
+// Not allowed for a special node
+if(isStatic() || isUpload())
+	exit;
+
+// Action on an existing file
+if(isset($_GET['id']) && !empty($_GET['id'])) {
+	$file_id = $_GET['id'];
+	
+	// Hack?
+	if(!isSafe($file_id)) {
+		header('Status: 406 Not Acceptable', true, 406);
+		exit('HTTP/1.1 406 Not Acceptable');
+	}
+	
+	// File does not exist
+	if(!file_exists(JAPPIX_BASE.'/store/conf/installed.xml')) {
+		header('Status: 404 Not Found', true, 404);
+		exit('HTTP/1.1 404 Not Found');
+	}
+	
+	// Remove a file
+	if(isset($_GET['action']) && ($_GET['action'] == 'remove')) {
+		
+	}
+	
+	// Receive a file
+	else {
+		
+	}
+}
+
+// Send a file
+else if() {
+	header('Content-Type: text/xml; charset=utf-8');
+}
+
+// Error?
+else {
+	header('Status: 400 Bad Request', true, 400);
+	exit('HTTP/1.1 400 Bad Request');
+}
+
+
+
+
+
+
+
+
+
+
+
+/* ////////////////////////// */
+// Everything is okay
+if((isset($_FILES['file']) && !empty($_FILES['file'])) && (isset($_POST['user']) && !empty($_POST['user'])) && (isset($_POST['location']) && !empty($_POST['location']))) {
+	// Get the user name
+	$user = $_POST['user'];
+	
+	// Get the file name
+	$tmp_filename = $_FILES['file']['tmp_name'];
+	$filename = $_FILES['file']['name'];
+	
+	// Get the location
+	if(HOST_UPLOAD)
+		$location = HOST_UPLOAD;
+	else
+		$location = $_POST['location'];
+	
+	// Forbidden file?
+	if(!isSafe($filename)) {
+		exit(
+'<jappix xmlns=\'jappix:file:post\'>
+	<error>forbidden-type</error>
+</jappix>'
+		);
+	}
+	
+	// Get the file new name
+	$ext = getFileExt($filename);
+	$new_name = preg_replace('/(^)(.+)(\.)(.+)($)/i', '$2', $filename);
+	
+	// Define some vars
+	$content_dir = JAPPIX_BASE.'/store/share/'.$user;
+	$security_file = $content_dir.'/index.html';
+	$name = sha1(time().$filename);
+	$path = $content_dir.'/'.$name.'.'.$ext;
+	$thumb_xml = '';
+	
+	// Create the user directory
+	if(!is_dir($content_dir)) {
+		mkdir($content_dir, 0777, true);
+		chmod($content_dir, 0777);
+	}
+	
+	// Create (or re-create) the security file
+	if(!file_exists($security_file))	
+		file_put_contents($security_file, securityHTML());
+	
+	// File upload error?
+	if(!is_uploaded_file($tmp_filename) || !move_uploaded_file($tmp_filename, $path)) {
+		exit(
+'<jappix xmlns=\'jappix:file:post\'>
+	<error>move-error</error>
+</jappix>'
+		);
+	}
+	
+	// Resize and compress if this is a JPEG file
+	if(preg_match('/^(jpg|jpeg|png|gif)$/i', $ext)) {
+		// Resize the image
+		resizeImage($path, $ext, 1024, 1024);
+		
+		// Copy the image
+		$thumb = $content_dir.'/'.$name.'_thumb.'.$ext;
+		copy($path, $thumb);
+		
+		// Create the thumbnail
+		if(resizeImage($thumb, $ext, 140, 105))
+			$thumb_xml = '<thumb>'.htmlspecialchars($location.'store/share/'.$user.'/'.$name.'_thumb.'.$ext).'</thumb>';
+	}
+	
+	// Return the path to the file
+	exit(
+'<jappix xmlns=\'jappix:file:post\'>
+	<href>'.htmlspecialchars($location.'store/share/'.$user.'/'.$name.'.'.$ext).'</href>
+	<title>'.htmlspecialchars($new_name).'</title>
+	<type>'.htmlspecialchars(getFileMIME($path)).'</type>
+	<length>'.htmlspecialchars(filesize($path)).'</length>
+	'.$thumb_xml.'
+</jappix>'
+	);
+}
+
+// Bad request error!
+exit(
+'<jappix xmlns=\'jappix:file:post\'>
+	<error>bad-request</error>
+</jappix>'
+);
+
+?>
