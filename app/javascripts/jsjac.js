5afd42ae20e1bb60918cb079707fdf5f812c37d2
Valérian Saliou <valerian@valeriansaliou.name>
2014-05-02 19:31:34 +0200

    fixes #441

--- app/javascripts/jsjac.js
+++ app/javascripts/jsjac.js
@@ -3601,10 +3601,6 @@ JSJaCConnection.prototype._handlePID = function(packet) {
   if (!packet.getID())
     return false;
 
-  if (packet.pType() != 'iq' ||
-         (packet.getType() != 'error' && packet.getType() != 'result'))
-    return false; 
-
   var jid = packet.getFrom() || this.jid;
 
   if (packet.getFrom() == this.domain)
