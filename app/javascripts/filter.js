4d01b607005ee8d5171374d2e324525a3725efdf
Valérian Saliou <valerian@valeriansaliou.name>
2014-05-26 19:10:08 +0200

    Enhance message filtering code

--- app/javascripts/filter.js
+++ app/javascripts/filter.js
@@ -20,6 +20,235 @@ var Filter = (function () {
     var self = {};
 
 
+    /* Constants */
+    self.message_regex = {
+        'commands': {
+            'me': /((^)|((.+)(>)))(\/me )([^<]+)/
+        },
+
+        'emotes': {
+            'angry': [
+                /(:-?@)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'bat': [
+                /(:-?\[)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'beer': [
+                /(\(B\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'biggrin': [
+                /((:-?D)|(XD))($|\s|<)/gi,
+                '$4'
+            ],
+
+            'blush': [
+                /(:-?\$)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'boy': [
+                /(\(Z\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'brflower': [
+                /(\(W\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'brheart': [
+                /((&lt;\/3)|(\(U\)))($|\s|<)/g,
+                '$4'
+            ],
+
+            'coffee': [
+                /(\(C\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'coolglasses': [
+                /((8-\))|(\(H\)))($|\s|<)/g,
+                '$4'
+            ],
+
+            'cry': [
+                /(:'-?\()($|\s|<)/gi,
+                '$2'
+            ],
+
+            'cuffs': [
+                /(\(%\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'devil': [
+                /(\]:-?&gt;)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'drink': [
+                /(\(D\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'flower': [
+                /(@}-&gt;--)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'frowning': [
+                /((:-?\/)|(:-?S))($|\s|<)/gi,
+                '$4'
+            ],
+
+            'girl': [
+                /(\(X\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'heart': [
+                /((&lt;3)|(\(L\)))($|\s|<)/g,
+                '$4'
+            ],
+
+            'hugleft': [
+                /(\(}\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'hugright': [
+                /(\({\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'kis': [
+                /(:-?{})($|\s|<)/gi,
+                '$2'
+            ],
+
+            'lamp': [
+                /(\(I\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'lion': [
+                /(:-?3)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'mail': [
+                /(\(E\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'moon': [
+                /(\(S\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'music': [
+                /(\(8\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'oh': [
+                /((=-?O)|(:-?O))($|\s|<)/gi,
+                '$4'
+            ],
+
+            'phone': [
+                /(\(T\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'photo': [
+                /(\(P\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'puke': [
+                /(:-?!)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'pussy': [
+                /(\(@\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'rainbow': [
+                /(\(R\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'smile': [
+                /(:-?\))($|\s|<)/gi,
+                '$2'
+            ],
+
+            'star': [
+                /(\(\*\))($|\s|<)/g,
+                 '$2'
+            ],
+
+            'stare': [
+                /(:-?\|)($|\s|<)/gi,
+                '$2'
+            ],
+
+            'thumbdown': [
+                /(\(N\))($|\s|<)/g,
+                 '$2'
+            ],
+
+            'thumbup': [
+                /(\(Y\))($|\s|<)/g,
+                '$2'
+            ],
+
+            'tongue': [
+                /(:-?P)($|\s|<)/gi,
+                 '$2'
+            ],
+
+            'unhappy': [
+                /(:-?\()($|\s|<)/gi,
+                '$2'
+            ],
+
+            'wink': [
+                /(;-?\))($|\s|<)/gi,
+                '$2'
+            ]
+
+        },
+
+        'formatting': {
+            'bold': [
+                /(^|\s|>|\()((\*)([^<>'"\*]+)(\*))($|\s|<|\))/gi,
+                '$1<b>$2</b>$6'
+            ],
+
+            'italic': [
+                /(^|\s|>|\()((\/)([^<>'"\/]+)(\/))($|\s|<|\))/gi,
+                '$1<em>$2</em>$6'
+            ],
+
+            'underline': [
+                /(^|\s|>|\()((_)([^<>'"_]+)(_))($|\s|<|\))/gi,
+                '$1<span style="text-decoration: underline;">$2</span>$6'
+            ]
+        }
+
+    };
+
+
     /**
      * Generates a given emoticon HTML code
      * @public
@@ -42,83 +271,61 @@ var Filter = (function () {
     /**
      * Filters a given message
      * @public
-     * @param {string} neutralMessage
+     * @param {string} message
      * @param {string} nick
      * @param {string} html_escape
      * @return {string}
      */
-    self.message = function(neutralMessage, nick, html_escape) {
+    self.message = function(message, nick, html_escape) {
 
         try {
-            var filteredMessage = neutralMessage;
+            var filtered = message;
             
             // We encode the HTML special chars
-            if(html_escape)
-                filteredMessage = filteredMessage.htmlEnc();
+            if(html_escape) {
+                filtered = filtered.htmlEnc();
+            }
             
             // /me command
-            filteredMessage = filteredMessage.replace(/((^)|((.+)(>)))(\/me )([^<]+)/, nick + ' $7')
+            filtered = filtered.replace(self.message_regex.commands.me, nick + ' $7')
             
             // We replace the smilies text into images
-            .replace(/(:-?@)($|\s|<)/gi, self.emoteImage('angry', '$1', '$2'))
-            .replace(/(:-?\[)($|\s|<)/gi, self.emoteImage('bat', '$1', '$2'))
-            .replace(/(\(B\))($|\s|<)/g, self.emoteImage('beer', '$1', '$2'))
-            .replace(/((:-?D)|(XD))($|\s|<)/gi, self.emoteImage('biggrin', '$1', '$4'))
-            .replace(/(:-?\$)($|\s|<)/gi, self.emoteImage('blush', '$1', '$2'))
-            .replace(/(\(Z\))($|\s|<)/g, self.emoteImage('boy', '$1', '$2'))
-            .replace(/(\(W\))($|\s|<)/g, self.emoteImage('brflower', '$1', '$2'))            
-            .replace(/((&lt;\/3)|(\(U\)))($|\s|<)/g, self.emoteImage('brheart', '$1', '$4'))         
-            .replace(/(\(C\))($|\s|<)/g, self.emoteImage('coffee', '$1', '$2'))          
-            .replace(/((8-\))|(\(H\)))($|\s|<)/g, self.emoteImage('coolglasses', '$1', '$4'))
-            .replace(/(:'-?\()($|\s|<)/gi, self.emoteImage('cry', '$1', '$2'))
-            .replace(/(\(%\))($|\s|<)/g, self.emoteImage('cuffs', '$1', '$2'))
-            .replace(/(\]:-?&gt;)($|\s|<)/gi, self.emoteImage('devil', '$1', '$2'))          
-            .replace(/(\(D\))($|\s|<)/g, self.emoteImage('drink', '$1', '$2'))
-            .replace(/(@}-&gt;--)($|\s|<)/gi, self.emoteImage('flower', '$1', '$2'))
-            .replace(/((:-?\/)|(:-?S))($|\s|<)/gi, self.emoteImage('frowning', '$1', '$4'))
-            .replace(/(\(X\))($|\s|<)/g, self.emoteImage('girl', '$1', '$2'))
-            .replace(/((&lt;3)|(\(L\)))($|\s|<)/g, self.emoteImage('heart', '$1', '$4'))
-            .replace(/(\(}\))($|\s|<)/g, self.emoteImage('hugleft', '$1', '$2'))         
-            .replace(/(\({\))($|\s|<)/g, self.emoteImage('hugright', '$1', '$2'))
-            .replace(/(:-?{})($|\s|<)/gi, self.emoteImage('kiss', '$1', '$2'))
-            .replace(/(\(I\))($|\s|<)/g, self.emoteImage('lamp', '$1', '$2'))
-            .replace(/(:-?3)($|\s|<)/gi, self.emoteImage('lion', '$1', '$2'))
-            .replace(/(\(E\))($|\s|<)/g, self.emoteImage('mail', '$1', '$2'))
-            .replace(/(\(S\))($|\s|<)/g, self.emoteImage('moon', '$1', '$2'))
-            .replace(/(\(8\))($|\s|<)/g, self.emoteImage('music', '$1', '$2'))
-            .replace(/((=-?O)|(:-?O))($|\s|<)/gi, self.emoteImage('oh', '$1', '$4'))
-            .replace(/(\(T\))($|\s|<)/g, self.emoteImage('phone', '$1', '$2'))
-            .replace(/(\(P\))($|\s|<)/g, self.emoteImage('photo', '$1', '$2'))
-            .replace(/(:-?!)($|\s|<)/gi, self.emoteImage('puke', '$1', '$2'))
-            .replace(/(\(@\))($|\s|<)/g, self.emoteImage('pussy', '$1', '$2'))
-            .replace(/(\(R\))($|\s|<)/g, self.emoteImage('rainbow', '$1', '$2'))
-            .replace(/(:-?\))($|\s|<)/gi, self.emoteImage('smile', '$1', '$2'))
-            .replace(/(\(\*\))($|\s|<)/g, self.emoteImage('star', '$1', '$2'))
-            .replace(/(:-?\|)($|\s|<)/gi, self.emoteImage('stare', '$1', '$2'))
-            .replace(/(\(N\))($|\s|<)/g, self.emoteImage('thumbdown', '$1', '$2'))
-            .replace(/(\(Y\))($|\s|<)/g, self.emoteImage('thumbup', '$1', '$2'))
-            .replace(/(:-?P)($|\s|<)/gi, self.emoteImage('tongue', '$1', '$2'))
-            .replace(/(:-?\()($|\s|<)/gi, self.emoteImage('unhappy', '$1', '$2'))
-            .replace(/(;-?\))($|\s|<)/gi, self.emoteImage('wink', '$1', '$2'))
+            var cur_emote;
+
+            for(var e in self.message_regex.emotes) {
+                cur_emote = self.message_regex.emotes[e];
+
+                filtered = filtered.replace(
+                    cur_emote[0],
+                    self.emoteImage(
+                        e,
+                        '$1',
+                        cur_emote[1]
+                    )
+                );
+            }
             
-            // Text in bold
-            .replace(/(^|\s|>|\()((\*)([^<>'"\*]+)(\*))($|\s|<|\))/gi, '$1<b>$2</b>$6')
-            
-            // Italic text
-            .replace(/(^|\s|>|\()((\/)([^<>'"\/]+)(\/))($|\s|<|\))/gi, '$1<em>$2</em>$6')
-            
-            // Underlined text
-            .replace(/(^|\s|>|\()((_)([^<>'"_]+)(_))($|\s|<|\))/gi, '$1<span style="text-decoration: underline;">$2</span>$6');
-            
+            // Text formatting
+            var cur_formatting;
+
+            for(var f in self.message_regex.formatting) {
+                cur_formatting = self.message_regex.formatting[f];
+
+                filtered = filtered.replace(
+                    cur_formatting[0],
+                    cur_formatting[1]
+                );
+            }
+
             // Add the links
             if(html_escape) {
-                filteredMessage = Links.apply(filteredMessage, 'desktop');
+                filtered = Links.apply(filtered, 'desktop');
             }
             
             // Filter integratebox links
-            filteredMessage = IntegrateBox.filter(filteredMessage);
+            filtered = IntegrateBox.filter(filtered);
             
-            return filteredMessage;
+            return filtered;
         } catch(e) {
             Console.error('Filter.message', e);
         }
@@ -136,70 +343,70 @@ var Filter = (function () {
 
         try {
             // Allowed elements array
-            var elements = new Array(
-                        'a',
-                        'abbr',
-                        'acronym',
-                            'address',
-                            'blockquote',
-                        'body',
-                        'br',
-                        'cite',
-                            'code',
-                            'dd',
-                            'dfn',
-                            'div',
-                            'dt',
-                            'em',
-                            'h1',
-                            'h2',
-                            'h3',
-                            'h4',
-                            'h5',
-                            'h6',
-                            'head',
-                            'html',
-                            'kbd',
-                            'li',
-                            'ol',
-                            'p',
-                            'pre',
-                            'q',
-                            'samp',
-                            'span',
-                            'strong',
-                            'title',
-                            'ul',
-                            'var'
-                           );
+            var elements = [
+                'a',
+                'abbr',
+                'acronym',
+                'address',
+                'blockquote',
+                'body',
+                'br',
+                'cite',
+                'code',
+                'dd',
+                'dfn',
+                'div',
+                'dt',
+                'em',
+                'h1',
+                'h2',
+                'h3',
+                'h4',
+                'h5',
+                'h6',
+                'head',
+                'html',
+                'kbd',
+                'li',
+                'ol',
+                'p',
+                'pre',
+                'q',
+                'samp',
+                'span',
+                'strong',
+                'title',
+                'ul',
+                'var'
+            ];
             
             // Allowed attributes array
-            var attributes = new Array(
-                           'accesskey',
-                           'alt',
-                           'charset',
-                           'cite',
-                           'class',
-                           'height',
-                           'href',
-                           'hreflang',
-                           'id',
-                           'longdesc',
-                           'profile',
-                           'rel',
-                           'rev',
-                           'src',
-                           'style',
-                           'tabindex',
-                           'title',
-                           'type',
-                           'uri',
-                           'version',
-                           'width',
-                           'xml:lang',
-                           'xmlns'
-                          );
-                          
+            var attributes = [
+               'accesskey',
+               'alt',
+               'charset',
+               'cite',
+               'class',
+               'height',
+               'href',
+               'hreflang',
+               'id',
+               'longdesc',
+               'profile',
+               'rel',
+               'rev',
+               'src',
+               'style',
+               'tabindex',
+               'title',
+               'type',
+               'uri',
+               'version',
+               'width',
+               'xml:lang',
+               'xmlns'
+            ];
+            
             // Check if Filter for XHTML-IM images is enabled
             if(DataStore.getDB(Connection.desktop_hash, 'options', 'no-xhtml-images') != '1') {
                 elements.push("img");
@@ -208,8 +415,9 @@ var Filter = (function () {
             // Remove forbidden elements
             $(code).find('html body *').each(function() {
                 // This element is not authorized
-                if(!Utils.existArrayValue(elements, (this).nodeName.toLowerCase()))
+                if(!Utils.existArrayValue(elements, (this).nodeName.toLowerCase())) {
                     $(this).remove();
+                }
             });
             
             // Remove forbidden attributes
@@ -226,8 +434,9 @@ var Filter = (function () {
                     var cVal = cAttr.value;
                     
                     // This attribute is not authorized, or contains JS code
-                    if(!Utils.existArrayValue(attributes, cName.toLowerCase()) || ((cVal.toLowerCase()).match(/(^|"|')javascript:/)))
+                    if(!Utils.existArrayValue(attributes, cName.toLowerCase()) || ((cVal.toLowerCase()).match(/(^|"|')javascript:/))) {
                         cSelector.removeAttr(cName);
+                    }
                 });
             });
             
